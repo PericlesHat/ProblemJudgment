fa1c6a92ad3aaad105ab331481f2daf5b600b571
KeyonC <67910132+PericlesHat@users.noreply.github.com>
2021-12-23 20:24:04 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -253,7 +253,7 @@ if __name__ == '__main__':
     parser.add_argument('--img-size', type=int, default=640,
                         help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float,
-                        default=0.70, help='object confidence threshold')
+                        default=0.99, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float,
                         default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='',
